54754ad20dbb7c4c9c0072205a97eb9d63f8dbd9
Helen Darden <hgdarden@gmail.com>
2020-06-10 18:11:37 -0400

    creating router routes in burger_controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,9 +2,9 @@ const express = require("express");
 const burger = require("../models/burger.js");
 
 const router = express.Router();
-
+//gets burgers listed in db
 router.get("/", function(req, res){
-    cat.all(function(data){
+    burger.all(function(data){
         var hbsObject = {
             burgers: data
         };
@@ -13,9 +13,29 @@ router.get("/", function(req, res){
     });
 });
 
-router.post("/api/cats", function(req, res){
-    
+//creates a new burger and adds to the list
+router.post("/api/burgers", function(req, res){
+    burger.create(["burger_name"], [req.body.name], function(result){
+        res.json({ id: result.insertId });
+    });
+});
+
+//when burger is devoured, adds to the devoured list
+router.put("/api/burgers/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+//updates burger to be devoured
+    burger.update({ devoured: req.body.devoured }, condition, function(result) {
+        if (result.changedRows == 0){
+            return res.status(404).end();
+        }else {
+            res.status(200).end();
+        }
+    }) 
 })
+
+
 
 
 
