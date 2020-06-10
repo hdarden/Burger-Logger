299750d4a385b273c1c33d6f00a0fa1f30bfb23f
Helen Darden <hgdarden@gmail.com>
2020-06-10 17:52:56 -0400

    updating burgers_controller, adding get route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,4 +3,21 @@ const burger = require("../models/burger.js");
 
 const router = express.Router();
 
-router.get("")
+router.get("/", function(req, res){
+    cat.all(function(data){
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/cats", function(req, res){
+    
+})
+
+
+
+
+module.exports = router;
