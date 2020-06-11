146871d83b05deb2a7efa7279cca7b8e83003885
Helen Darden <hgdarden@gmail.com>
2020-06-11 19:32:37 -0400

    adding JAWS connection

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,22 @@
 //connecting to mysql
 var mysql = require("mysql");
+var connection;
 
 //creating connection
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "rootroot",
     database: "burgers_db"
   });
-  //connecting
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-  
-    console.log("connected as id " + connection.threadId);
-  });
+};
 
+  //connecting
+  connection.connect();
 
 //exporting connection
 module.exports = connection;
