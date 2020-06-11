ea49afc7eb935adbf194030b839192f6eb4c9da4
Helen Darden <hgdarden@gmail.com>
2020-06-10 21:20:39 -0400

    updating orm

--- config/orm.js
+++ config/orm.js
@@ -1,24 +1,102 @@
-const connection = require("./connection");
+//imports MySQL connection
+const connection = require("./connection.js");
+
+//======================Helper function==================================
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
+  
+  // Helper function to convert object key/value pairs to SQL syntax
+  function objToSql(ob) {
+    var arr = [];
+  
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+  }
+//======================== ORM ===========================================
 
 const orm = {
     selectAll: function(tableInput, cb){
-      connection.query(`SELECT * FROM ${tableInput}`, function(err, res){
-          if (err) throw err;
-            cb(res); 
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          cb(result);
       })
     },
 
-    insertOne: function(tableInput, burgerName, colName, cb) {
+    /* insertOne: function(tableInput, burgerName, colName, cb) {
         connection.query(`INSERT INTO ${tableInput} (${colName}) VALUES (${burgerName})`, function(err, res){
             if(err) throw err;
             cb(res);
         })
-    },
+    }, */
 
-    updateOne: function(tableInput, objColVals, condition, cb){
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+    
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
+
+   /*  updateOne: function(tableInput, objColVals, condition, cb){
         connection.query(`UPDATE `)
-    } 
+    }  */
+
+    updateOne: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
     
 }
-
+//required by models/burger.js
 module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -4,14 +4,17 @@ const burger = {
     selectAll: function(cb){
     orm.selectAll("burgers", function(res){
         cb(res);
-    });
-
+        });
     },
+
     insertOne: function(cb, burgerName){
         orm.insertOne("burgers", burgerName, "burger_name", function(res){
             cb(res);
-        })
+        });
+    },
 
+    updateOne: function(,cb){
+        orm.updateOne()
     }
 
 }
