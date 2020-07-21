4527f82273e539e404b6ee2817333fbc63a04e29
Helen Darden <hgdarden@gmail.com>
2020-07-21 13:24:29 -0400

    setting up delete route

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const burger = require("../models/burger.js");
-
-//establiching router
 const router = express.Router();
 
 //gets burgers already listed in db
@@ -10,14 +8,14 @@ router.get("/", function(req, res){
         var hbsObject = {
             burgers: data
         };
-        console.log(hbsObject);
+        //console.log(hbsObject);
         res.render("index", hbsObject);
     });
 });
 
 //creates a new burger and adds to the list
 router.post("/api/burgers", function(req, res){
-    console.log(req.body)
+     //console.log(req.body)
     burger.insertOne(["burger_name"], [req.body.burger_name], function(result){
         res.json({ id: result.insertId });
     });
@@ -26,19 +24,29 @@ router.post("/api/burgers", function(req, res){
 //when burger is devoured, adds to the devoured list
 router.put("/api/burgers/:id", function(req, res){
     var condition = "id = " + req.params.id;
-    console.log(condition);
+    /* console.log(condition);
     console.log("condition", condition);
-    console.log(req.body);
+    console.log(req.body); */
 //updates burger to be devoured
     burger.updateOne(req.params.id, function(result) {
         if (result.changedRows == 0){
             return res.status(404).end();
         }else {
             res.status(200).end();
-        }
-    }) 
-})
+        };
+    }); 
+});
 
+router.delete("/api/burgers/:id", function(req, res){
+    var condition = "id = " + req.params.id;
 
+    burger.delte(condition, function(result) {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        };
+    });
+});
 //exporting to views
 module.exports = router;
